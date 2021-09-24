8440ceb1a3fa3f3e3625227005bb66289af68d16
Colgan <dcolgan@ksu.edu>
2021-09-24 10:32:31 -0500

    Class Iterations

--- first_largest.py
+++ first_largest.py
@@ -2,14 +2,20 @@
 Python program to find the largest element and its location.
 """
 
-def largest_element(a):
-    """ Return the largest element of a sequence a.
-    """
-
-    return None
+def largest_element(a,loc=False):
+    """ Return the largest element of a sequence a."""
+    maxval=a[0]
+    maxloc=0
+    for i in range(1,len(a)):
+        if a[i] > maxval:
+                maxval = a[i]
+                maxloc = i
+    if loc == True:
+        return maxval,maxloc
+    return maxval
 
 
 if __name__ == "__main__":
 
-    a = [1,2,3,2,1]
+    a = [5,3,4,1,2]
     print("Largest element is {:}".format(largest_element(a)))
--- tests.py
+++ tests.py
@@ -1,13 +1,19 @@
 import unittest
 
-from first_largest import largest_element
+from first_largest import *
 
 class FirstLargest(unittest.TestCase):
 
     def test_largest_is_first(self):
-        given = [1,2,3,2,1]
-        expect = 3
+        given = [5,3,4,1,2]
+        expect = 5
         got = largest_element(given)
+        self.assertEqual(got, expect)
+   
+    def test_largest_is_first_with_loc(self):
+        given = [5,3,4,1,2]
+        expect = 5, 0
+        got = largest_element(given, loc=True)
         self.assertEqual(got, expect)
    
     """ ADD MORE  """
